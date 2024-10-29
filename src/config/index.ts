d18d1a9e53e30cd971763faab665f928efa31f8e
mbvgua <mbuguadouglas17@gmail.com>
2024-10-29 10:44:49 +0300

    connect the application to the mssql-server database

---
+++ src/config/index.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+
+// configure the path to the environmental variables
+dotenv.config({ path:path.resolve(__dirname, '../../.env') })
+
+// connecting the the sql-server
+export const sqlConfig = {
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PWD,
+    server: 'localhost',
+    poll: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
+    options: {
+        encrypt: false,
+        trustServerCertificate: true
+    }
+
+}
